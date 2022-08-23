82fe89d777b95a754d476b45cbf7199eed2e6341
fahim-abser <fahimabser0123@gmail.com>
2022-08-23 16:38:00 +0600

    style and js update

--- js/calculation.js
+++ js/calculation.js
@@ -2,20 +2,16 @@ const nameArray = [];
 function displayName() {
     const totalPlayers = document.getElementById("selected-player");
     totalPlayers.innerText = nameArray.length;
-
     const nameContainer = document.getElementById("player-name");
     nameContainer.textContent = '';
 
-
     for (let i = 0; i < nameArray.length; i++) {
-
 
         const tr = document.createElement("tr");
         tr.innerHTML = `
         <th><h4 style="margin-right:25px">${i + 1}.</h4></th>
         <td style="text-align:left; padding:10px"> <h4>${nameArray[i]}</h4></td>`;
         nameContainer.appendChild(tr);
-
     }
 }
 
@@ -28,16 +24,10 @@ function selectItem(name) {
     else {
         displayName();
     }
-
-
 }
-
 document.getElementById('calculate-btn').addEventListener
     ('click', function () {
-
         const playerExpenseField = getInputValue('budget-field');
-
-
         if (isNaN(playerExpenseField) == true) {
             alert('Please enter a valid input!')
         }
@@ -45,14 +35,12 @@ document.getElementById('calculate-btn').addEventListener
             alert('Please enter a positive number!')
         }
         else {
-
             const players = nameArray.length;
             const totalBudget = players * playerExpenseField;
             console.log(totalBudget);
             updateValues('player-expense-field', totalBudget);
             firstTotal = totalBudget;
         }
-
         document.getElementById('budget-field').value = '';
     })
 
@@ -60,7 +48,6 @@ document.getElementById('calculate-btn').addEventListener
 
 document.getElementById('calculate-total-btn').addEventListener
     ('click', function () {
-
         const managerExpenseField = getInputValue('manager-expense-field');
         const coachExpenseField = getInputValue('coach-expense-field');
         const playerExpenseField = document.getElementById('player-expense-field').innerText;
@@ -76,21 +63,17 @@ document.getElementById('calculate-total-btn').addEventListener
 
             console.log(playerExpenseField);
             updateValues('final-amount', totalExpense)
-
         }
         document.getElementById('manager-expense-field').value = '';
         document.getElementById('coach-expense-field').value = '';
-
     }
     )
 
 function getInputValue(inputId) {
-
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
     const amountValue = parseFloat(inputAmountText);
     return amountValue;
-
 }
 function updateValues(updateText, value) {
 
